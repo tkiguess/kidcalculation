aaa43b5250312a4b4b54ca8c982670885f975eb1
Tafkir Ahmed <ahmedtafkir@gmail.com>
2018-07-12 15:08:42 -0400

    giant marshmallow

---
+++ src/kidprogram.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class kidprogram {
+
+    public static void main (String[] args){
+
+        Scanner num= new Scanner(System.in);
+
+        System.out.println("Gimme a number big boi ");
+         int bigfoot = num.nextInt();
+
+        System.out.println((bigfoot +5)*2-7 + " Here you go big boi I calculated some mathimaticals for you");
+
+
+
+    }
+}
